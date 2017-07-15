d9249b323872772bee2b1c44eb2094e0aebd7ebe
Christopher Murphy <cmhurripsh@gmail.com>
2017-07-15 10:40:02 -0400

    Sort current assignments chronologically, select first one

--- src/edu/clarkson/autograder/client/Data.java
+++ src/edu/clarkson/autograder/client/Data.java
@@ -13,100 +13,97 @@ import edu.clarkson.autograder.client.objects.Course;
  */
 public class Data {
 
-    public static final int ID_TOKEN_WIDTH = 6;
+	public static final int ID_TOKEN_WIDTH = 6;
 
-    public static String formatIdToken(int id) {
-        // Attempts to provide identical functionality as:
-        // String.format("%0" + ID_TOKEN_WIDTH + "d", id);
-        int unpadded_length = ("" + id).length();
-        String padding = "";
-        for (int i = 0; i < ID_TOKEN_WIDTH - unpadded_length; ++i)
-            padding += "0";
-        return padding + id;
-    }
+	public static String formatIdToken(int id) {
+		// Attempts to provide identical functionality as:
+		// String.format("%0" + ID_TOKEN_WIDTH + "d", id);
+		int unpadded_length = ("" + id).length();
+		String padding = "";
+		for (int i = 0; i < ID_TOKEN_WIDTH - unpadded_length; ++i)
+			padding += "0";
+		return padding + id;
+	}
 
-    /**
-     * List of courses accessible by user
-     * 
-     * <li>ME310
-     * <li>ES436
-     * <li>AA100
-     * <li>Course A <i>(invisible)</i>
-     * <li>Course B
-     */
-    private static final List<Course> courses = new ArrayList<>();
-    static {
-        /*
-         * Create courses
-         */
-        courses.add(new Course(1, "ME310", "Thermodynamics - course description", true));
-        courses.add(new Course(2, "ES436", "Climate Change - course description", true));
-        courses.add(new Course(3, "AA100", "Generic Course - course description", true));
-        courses.add(new Course(4, "Course A", "Generic Course - course description", false));
-        courses.add(new Course(5, "Course B",
-                "Generic Course - course description is especially long in this case."
-                        + " We just keep going and going and going and going and going and "
-                        + "This course description is especially long in this case.",
-                true));
-    }
+	/**
+	 * List of courses accessible by user
+	 * 
+	 * <li>ME310
+	 * <li>ES436
+	 * <li>AA100
+	 * <li>Course A <i>(invisible)</i>
+	 * <li>Course B
+	 */
+	private static final List<Course> courses = new ArrayList<>();
+	static {
+		/*
+		 * Create courses
+		 */
+		courses.add(new Course(1, "ME310", "Thermodynamics - course description", true));
+		courses.add(new Course(2, "ES436", "Climate Change - course description", true));
+		courses.add(new Course(3, "AA100", "Generic Course - course description", true));
+		courses.add(new Course(4, "Course A", "Generic Course - course description", false));
+		courses.add(new Course(5, "Course B",
+		        "Generic Course - course description is especially long in this case."
+		                + " We just keep going and going and going and going and going and "
+		                + "This course description is especially long in this case.",
+		        true));
+	}
 
-    /**
+	/**
 	 * List of accessible assignments
 	 */
-    private static final List<Assignment> assignments = new ArrayList<>();
-    static {
-        /*
-         * Create assignments for every course
-         */
-        int number_of_assignments = 12;
-        for (Course c : courses) {
-            for (int a_num = 0; a_num < number_of_assignments; a_num++) {
-                // open date 1498795200000 is 06/30/2017 04:00 AM GMT
-                // One day increment: 86400000
-                // close date is between -10 and +2 days from current date
-                assignments.add(new Assignment(a_num, c.getId(), true,
-                        "Assignment Title "
-                                + (a_num + 1),
-                        new Date(1498795200000L),
-                        new Date(10000 + (new Date()).getTime() - (86400000L * (number_of_assignments - 2 - a_num)))));
-            }
-        }
-    }
+	private static final List<Assignment> assignments = new ArrayList<>();
+	static {
+		/*
+		 * Create assignments for every course
+		 */
+		int number_of_assignments = 12;
+		for (Course c : courses) {
+			for (int a_num = 0; a_num < number_of_assignments; a_num++) {
+				// open date 1498795200000 is 06/30/2017 04:00 AM GMT
+				// One day increment: 86400000
+				// close date is between -10 and +2 days from current date
+				assignments.add(new Assignment(a_num, c.getId(), true, "Assignment Title " + (a_num + 1),
+				        new Date(1498795200000L),
+				        new Date(120000 + (new Date()).getTime() - (86400000L * (number_of_assignments - 2 - a_num)))));
+			}
+		}
+	}
 
+	/*
+	 * 
+	 * 
+	 * Access functions
+	 * 
+	 * 
+	 */
 
-    /*
-     * 
-     * 
-     * Access functions
-     * 
-     * 
-     */
+	public static List<Course> getCourses() {
+		return courses;
+	}
 
-    public static List<Course> getCourses() {
-        return courses;
-    }
-    
-    /**
-     * 
-     * Return Course data associated with the course ID or null if course does
-     * not exist.
-     */
-    public static Course getCourseFor(int courseId) {
-        for (Course course : courses) {
-            if (course.getId() == courseId) {
-                return course;
-            }
-        }
-        return null;
-    }
+	/**
+	 * 
+	 * Return Course data associated with the course ID or null if course does
+	 * not exist.
+	 */
+	public static Course getCourseFor(int courseId) {
+		for (Course course : courses) {
+			if (course.getId() == courseId) {
+				return course;
+			}
+		}
+		return null;
+	}
 
-    public static List<Assignment> getAssignmentsFor(int courseId) {
-        List<Assignment> sublist = new ArrayList<>();
-        for (Assignment a : assignments) {
-            if (a.getcId() == courseId) {
-                sublist.add(a);
-            }
-        }
-        return sublist;
-    }
+	public static List<Assignment> getAssignmentsFor(int courseId) {
+		List<Assignment> sublist = new ArrayList<>();
+		for (Assignment a : assignments) {
+			if (a.getcId() == courseId) {
+				sublist.add(a);
+			}
+		}
+		return sublist;
+	}
 }
--- src/edu/clarkson/autograder/client/pages/CoursePage.java
+++ src/edu/clarkson/autograder/client/pages/CoursePage.java
@@ -28,154 +28,165 @@ import edu.clarkson.autograder.client.widgets.Listing;
  */
 public class CoursePage extends Content {
 
-    private Course course;
-    private List<Assignment> assignments = null;
+	private Course course;
+	private List<Assignment> assignments = null;
 
-    private FlexTable assignmentsTable = new FlexTable();
-    // TODO make persistance selection of listing
-    private Listing activeAssignmentListing;
+	private FlexTable assignmentsTable = new FlexTable();
+	// TODO make persistance selection of listing
+	private Listing activeAssignmentListing;
 
-    /**
-     * Load CoursePage with specified assignment selection.
-     */
-    public CoursePage(Course course, int initialSelectedAssignmentId) {
+	/**
+	 * Load CoursePage with specified assignment selection.
+	 */
+	public CoursePage(Course course, int preselectedAssignmentId) {
 
-        this.course = course;
-        loadAssignments();
-        int selectedAssignmentId = initialSelectedAssignmentId;
-        if (initialSelectedAssignmentId == -1) {
-            selectedAssignmentId = selectDefaultAssignmentId();
-        }
+		this.course = course;
+		loadAssignments();
 
-        // create assignment listings and set selected listing
-        List<Listing> currentListings = new ArrayList<>();
-        List<Listing> pastListings = new ArrayList<>();
-        Date date = new Date();
-        for (Assignment assignment : assignments) {
-            Listing listing = new Listing(assignment);
-            // set selected assignment
-            if (assignment.getId() == selectedAssignmentId) {
-                activeAssignmentListing = listing;
-                activeAssignmentListing.setSelected(true);
-            }
+		// create assignment listings and highlight one
+		List<Listing> currentListings = new ArrayList<>();
+		List<Listing> pastListings = new ArrayList<>();
+		Date date = new Date();
+		for (Assignment assignment : assignments) {
+			Listing listing = new Listing(assignment);
+			// set selected assignment
+			if (assignment.getId() == preselectedAssignmentId) {
+				activeAssignmentListing = listing;
+				activeAssignmentListing.setSelected(true);
+			}
+			// add listing to either "current" or "past" list
+			if (date.before(assignment.getCloseTime())) {
+				currentListings.add(listing);
+			} else {
+				pastListings.add(listing);
+			}
+		}
+		if (!currentListings.isEmpty()) {
+			// reorder currentListings chronologically by reversing order
+			Listing temp;
+			int start = 0, end = currentListings.size() - 1;
+			while (start < end) {
+				temp = currentListings.get(start);
+				currentListings.set(start, currentListings.get(end));
+				currentListings.set(end, temp);
+				start++;
+				end--;
+			}
 
-            // add listing to either "current" or "past" list
-            if (date.before(assignment.getCloseTime())) {
-                currentListings.add(listing);
-            } else {
-                pastListings.add(listing);
-            }
-        }
+			// select assignment with closest future due date, if none selected
+			if (activeAssignmentListing == null) {
+				// in chronological order, first listing is closest to present
+				activeAssignmentListing = currentListings.get(0);
+				activeAssignmentListing.setSelected(true);
+			}
+		}
 
-        // Create page header
-        Label pageTitle = new Label(course.getTitle());
-        pageTitle.addStyleName("pageTitle");
-        Label subTitle = new Label(course.getDescription());
-        subTitle.addStyleName("pageSubTitle");
-        VerticalPanel pageHeader = new VerticalPanel();
-        pageHeader.add(pageTitle);
-        pageHeader.add(subTitle);
-        // TODO center page header
-        pageHeader.addStyleName("coursePageHeader");
+		// Create page header
+		Label pageTitle = new Label(course.getTitle());
+		pageTitle.addStyleName("pageTitle");
+		Label subTitle = new Label(course.getDescription());
+		subTitle.addStyleName("pageSubTitle");
+		VerticalPanel pageHeader = new VerticalPanel();
+		pageHeader.add(pageTitle);
+		pageHeader.add(subTitle);
+		// TODO center page header
+		pageHeader.addStyleName("coursePageHeader");
 
-        // Create assignments table
-        // TODO: collapse assignment table to the left (using nifty chevrons)
-        assignmentsTable.setCellSpacing(6);
-        // current assignments
-        assignmentsTable.setHTML(0, 0, "Current Assignments:   (" + currentListings.size() + ")");
-        assignmentsTable.getCellFormatter().setStyleName(assignmentsTable.getRowCount(), 0, "assignmentsTableHeader");
-        for (Listing listing : currentListings) {
-            assignmentsTable.setWidget(assignmentsTable.getRowCount(), 0, listing);
-        }
-        // past assignments
-        assignmentsTable.setHTML(assignmentsTable.getRowCount(), 0,
-                "Past Assignments:   (" + pastListings.size() + ")");
-        assignmentsTable.getCellFormatter().setStyleName(assignmentsTable.getRowCount(), 0, "assignmentsTableHeader");
-        for (Listing listing : pastListings) {
-            assignmentsTable.setWidget(assignmentsTable.getRowCount(), 0, listing);
-        }
+		// Create assignments table
+		// TODO: collapse assignment table to the left (using nifty chevrons)
+		assignmentsTable.setCellSpacing(6);
+		// current assignments
+		assignmentsTable.setHTML(0, 0, "Current Assignments:   (" + currentListings.size() + ")");
+		assignmentsTable.getCellFormatter().setStyleName(0, 0, "assignmentsTableHeader");
+		for (Listing listing : currentListings) {
+			assignmentsTable.setWidget(assignmentsTable.getRowCount(), 0, listing);
+		}
+		// past assignments
+		int labelRow = assignmentsTable.getRowCount();
+		assignmentsTable.setHTML(labelRow, 0, "Past Assignments:   (" + pastListings.size() + ")");
+		assignmentsTable.getCellFormatter().addStyleName(labelRow, 0, "assignmentsTableHeader");
+		for (Listing listing : pastListings) {
+			assignmentsTable.setWidget(assignmentsTable.getRowCount(), 0, listing);
+		}
 
-        assignmentsTable.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                Cell c = assignmentsTable.getCellForEvent(event);
-                if (c != null) {
-                    Widget widget = assignmentsTable.getWidget(c.getRowIndex(), 0);
-                    if (widget instanceof Listing) {
-                        setSelectedAssignment((Listing) widget);
-                    }
-                }
-            }
-        });
+		assignmentsTable.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				Cell c = assignmentsTable.getCellForEvent(event);
+				if (c != null) {
+					Widget widget = assignmentsTable.getWidget(c.getRowIndex(), 0);
+					if (widget instanceof Listing) {
+						setSelectedAssignment((Listing) widget);
+					}
+				}
+			}
+		});
 
-        assignmentsTable.addStyleName("assignmentsTable");
+		assignmentsTable.addStyleName("assignmentsTable");
 
-        // Question layout pane
-        // TODO put assignment view template in question layout pane
-        // TODO populate assignment view template with question data from DB
-        String contentString = "<p>";
-        for (int i = 0; i < 200; ++i) {
-            contentString += "line " + (i + 1) + " Assignment ID=" + selectedAssignmentId + " Content<br>";
-            if (i == 10)
-                for (int j = 0; j < 50; ++j)
-                    contentString += "Assignment Content ";
-        }
-        contentString += "</p>";
-        HTML assignmentContent = new HTML(contentString);
-        ScrollPanel scroller = new ScrollPanel(assignmentContent);
-        scroller.setSize("400px", "100px");
+		// TODO put assignment view template in question layout pane
+		// TODO populate assignment view template with question data from DB
+		// TODO if no current assignments display special assignment content
+		// Question layout pane
+		String contentString = "<p>";
+		for (int i = 0; i < 200; ++i) {
+			contentString += "line " + (i + 1) + " Assignment ID=" + activeAssignmentListing.getContent().getId()
+			        + " Content<br>";
+			if (i == 10)
+				for (int j = 0; j < 50; ++j)
+					contentString += "Assignment Content ";
+		}
+		contentString += "</p>";
+		HTML assignmentContent = new HTML(contentString);
+		ScrollPanel scroller = new ScrollPanel(assignmentContent);
+		scroller.setSize("400px", "100px");
 
-        HorizontalPanel pageContentPane = new HorizontalPanel();
-        pageContentPane.add(assignmentsTable);
-        pageContentPane.add(assignmentContent);
-        assignmentContent.addStyleName("assignmentContent");
-        pageContentPane.addStyleName("assignmentPageContentPane");
+		HorizontalPanel pageContentPane = new HorizontalPanel();
+		pageContentPane.add(assignmentsTable);
+		pageContentPane.add(assignmentContent);
+		assignmentContent.addStyleName("assignmentContent");
+		pageContentPane.addStyleName("assignmentPageContentPane");
 
-        VerticalPanel pageTopLayout = new VerticalPanel();
-        pageTopLayout.add(pageHeader);
-        pageTopLayout.add(pageContentPane);
+		VerticalPanel pageTopLayout = new VerticalPanel();
+		pageTopLayout.add(pageHeader);
+		pageTopLayout.add(pageContentPane);
 
-        // Add page to app
-        initWidget(pageTopLayout);
-    }
+		// Add page to app
+		initWidget(pageTopLayout);
+	}
 
-    public void setSelectedAssignment(Listing selection) {
-        // ignore selecting same listing twice in a row
-        if (selection == activeAssignmentListing) {
+	public void setSelectedAssignment(Listing selection) {
+		// ignore selecting same listing twice in a row
+		if (selection == activeAssignmentListing) {
 
-            // remove previous selection
-            activeAssignmentListing.setSelected(false);
+			// remove previous selection
+			activeAssignmentListing.setSelected(false);
 
-            // set current selection
-            activeAssignmentListing = selection;
-            activeAssignmentListing.setSelected(true);
+			// set current selection
+			activeAssignmentListing = selection;
+			activeAssignmentListing.setSelected(true);
 
-            // TODO update assignment content
-        }
-    }
+			// TODO update assignment content
+		}
+	}
 
-    @Override
-    public String getPrimaryStyleName() {
-        return "coursePage";
-    }
+	@Override
+	public String getPrimaryStyleName() {
+		return "coursePage";
+	}
 
-    /**
-     * Load a reverse-chronologically sorted copy of course assignments
-     */
-    private void loadAssignments() {
-        assignments = Data.getAssignmentsFor(course.getId());
+	/**
+	 * Load a reverse-chronologically sorted copy of course assignments
+	 */
+	private void loadAssignments() {
+		assignments = Data.getAssignmentsFor(course.getId());
 
-        // sort assignments by date, future to past
-        Collections.sort(assignments, new Comparator<Assignment>() {
-            @Override
-            public int compare(Assignment a1, Assignment a2) {
-                return a2.getCloseTime().compareTo(a1.getCloseTime());
-            }
-        });
-    }
-
-    private int selectDefaultAssignmentId() {
-        // TODO: choose default assignment as closest future close time
-        return assignments.get(0).getId();
-    }
+		// sort assignments by date, future to past
+		Collections.sort(assignments, new Comparator<Assignment>() {
+			@Override
+			public int compare(Assignment a1, Assignment a2) {
+				return a2.getCloseTime().compareTo(a1.getCloseTime());
+			}
+		});
+	}
 }
