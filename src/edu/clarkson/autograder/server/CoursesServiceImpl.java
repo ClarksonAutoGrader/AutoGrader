443d5e3a07f38105d088b69b3a62dc2b03a37ee6
Dillon Clapp <djclapp97@gmail.com>
2017-10-29 18:05:28 -0400

    #20 - Added database query capability and JDBC to server
    # WARNING: head commit changed in the meantime
    
    Merge branch
    '11-as-a-developer-i-want-the-client-to-fetch-course-data-from-a-server-side-database'
    of git@gitlab.com:ClarksonUniversity/autograder/autograder-gwt.git into
    11-as-a-developer-i-want-the-client-to-fetch-course-data-from-a-server-side-database
    
    Conflicts:
    	src/edu/clarkson/autograder/client/objects/Assignment.java

--- src/edu/clarkson/autograder/server/CoursesServiceImpl.java
+++ src/edu/clarkson/autograder/server/CoursesServiceImpl.java
@@ -2,6 +2,9 @@ package edu.clarkson.autograder.server;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
@@ -11,25 +14,36 @@ import edu.clarkson.autograder.client.services.CoursesService;
 @SuppressWarnings("serial")
 public class CoursesServiceImpl extends RemoteServiceServlet implements CoursesService {
 	
+	private static ConsoleHandler LOG = new ConsoleHandler();
+	
+	private DatabaseQuery db;
+	
 	public List<Course> fetchCourses(){
-
-		final List<Course> courses = new ArrayList<>();
-		{
-			/*
-			 * Create courses
-			 */
-			courses.add(new Course(1, "ME310", "Thermodynamics - course description", true));
-			courses.add(new Course(2, "ES436", "Climate Change - course description", true));
-			courses.add(new Course(3, "AA100", "Generic Course - course description", true));
-			courses.add(new Course(4, "Course S", "Generic Course - course description", false));
-			courses.add(new Course(5, "Course C",
-			        "Generic Course - course description is especially long in this case."
-			                + " We just keep going and going and going and going and going and "
-			                + "This course description is especially long in this case.",
-			        true));
-		}
 		
-		return courses;
+		LOG.publish(new LogRecord(Level.INFO, "#fetchCourses"));
+		
+		db = new DatabaseQuery();
+		return db.queryCourses();
+		
+//		final List<Course> courses = new ArrayList<>();
+//		{
+//			/*
+//			 * Create courses
+//			 */
+//			courses.add(new Course(1, "ME310", "Thermodynamics - course description", true));
+//			courses.add(new Course(2, "ES436", "Climate Change - course description", true));
+//			courses.add(new Course(3, "AA100", "Generic Course - course description", true));
+//			courses.add(new Course(4, "Course S", "Generic Course - course description", false));
+//			courses.add(new Course(5, "Course C",
+//			        "Generic Course - course description is especially long in this case."
+//			                + " We just keep going and going and going and going and going and "
+//			                + "This course description is especially long in this case.",
+//			        true));
+//		}
+//		
+//		
+//		
+//		return courses;
 	}
 
 }
