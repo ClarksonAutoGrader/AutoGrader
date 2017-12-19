bd439aad9634c354267eb91ea3cbc84186bb5189
Ryan Wood <woodrj@clarkson.edu>
2017-12-19 18:20:22 -0500

    #4 - Added username async servlet mapping

--- src/edu/clarkson/autograder/client/services/UsernameService.java
+++ src/edu/clarkson/autograder/client/services/UsernameService.java
@@ -3,7 +3,7 @@ package edu.clarkson.autograder.client.services;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
-@RemoteServiceRelativePath("userName")
+@RemoteServiceRelativePath("username")
 
 //Gets current CAS session username
 public interface UsernameService extends RemoteService {
