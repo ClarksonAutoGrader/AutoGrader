12aa2c651c6bdda65ac5862989f089e1b61c2824
Ryan Wood <woodrj@clarkson.edu>
2017-12-23 19:06:08 -0500

    #5 - progress on GradebookPage class

--- src/edu/clarkson/autograder/client/pages/GradebookPage.java
+++ src/edu/clarkson/autograder/client/pages/GradebookPage.java
@@ -1,5 +1,32 @@
 package edu.clarkson.autograder.client.pages;
 
-public class GradebookPage {
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 
+import com.google.gwt.logging.client.SimpleRemoteLogHandler;
+import com.google.gwt.user.client.ui.Label;
+
+import edu.clarkson.autograder.client.widgets.Content;
+
+public class GradebookPage extends Content {
+	
+	private static SimpleRemoteLogHandler LOG = new SimpleRemoteLogHandler();
+	
+	private int courseID;
+	
+	public GradebookPage(int courseID) {
+		LOG.publish(new LogRecord(Level.INFO, "GradebookPage#<init> - courseId=" + courseID));
+		this.courseID = courseID;
+		
+		Label pageTitle = new Label(edu.clarkson.autograder.client.Autograder.tempDebugCourseNameSelected);
+		pageTitle.addStyleName("gradebookPageHeader");
+		
+		
+	}
+	
+	@Override
+	public String getPrimaryStyleName() {
+		return "gradebookpage";
+	}
+	
 }
