184d93c81dcb1137a448d045271dbd6121f88a58
Christopher Murphy <murphycd@clarkson.edu>
2017-12-26 14:58:52 -0500

    #26 Implement service: SelectedProblemDataService

---
+++ src/edu/clarkson/autograder/server/SelectedProblemDataServiceImpl.java
@@ -0,0 +1,33 @@
+package edu.clarkson.autograder.server;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import edu.clarkson.autograder.client.objects.Problem;
+import edu.clarkson.autograder.client.objects.ProblemData;
+import edu.clarkson.autograder.client.services.SelectedProblemDataService;
+
+@SuppressWarnings("serial")
+public class SelectedProblemDataServiceImpl extends RemoteServiceServlet implements SelectedProblemDataService {
+
+	private static ConsoleHandler LOG = new ConsoleHandler();
+
+	@Override
+	public ProblemData fetchProblemData(int problemId) {
+		LOG.publish(new LogRecord(Level.INFO, "AssignmentProblemTreeDataImpl#fetchTreeData - begin"));
+
+		// TODO uncomment and use
+//		Database db = new Database();
+//		ProblemData data = db.querySelectedProblemData(problemId);
+		
+		// TODO delete this debug line
+		ProblemData data = new ProblemData(new Problem(7, 8, "MyTitle", 35.5, 50), "Sample body text.", 4, 5);
+
+		LOG.publish(new LogRecord(Level.INFO, "AssignmentProblemTreeDataImpl#fetchTreeData - end"));
+		return data;
+	}
+	
+}
