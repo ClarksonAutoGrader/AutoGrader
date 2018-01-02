c891a4360722179e9b376ff607a55a1ddce9a255
Christopher Murphy <murphycd@clarkson.edu>
2018-01-02 13:18:05 -0500

    #25 add new images for question correctness and info button

--- src/edu/clarkson/autograder/client/AutograderResources.java
+++ src/edu/clarkson/autograder/client/AutograderResources.java
@@ -1,5 +1,6 @@
 package edu.clarkson.autograder.client;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 
@@ -7,5 +8,17 @@ import com.google.gwt.resources.client.ImageResource;
  * The images and styles used throughout the Autograder.
  */
 public interface AutograderResources extends ClientBundle {
+
+	/**
+	 * The static images used throughout the Autograder.
+	 */
+	public static final AutograderResources INSTANCE = GWT.create(AutograderResources.class);
+
 	ImageResource loading();
+
+	ImageResource greenCheck();
+
+	ImageResource redCross();
+
+	ImageResource info();
 }
