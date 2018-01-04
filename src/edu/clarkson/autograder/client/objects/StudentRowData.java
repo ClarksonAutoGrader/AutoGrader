7c91c603b2f0edc37ebcdcb0afdff71b9abde6d9
Ryan Wood <woodrj@clarkson.edu>
2018-01-03 22:35:55 -0500

    #5 - progress on abstraction of gradebookpage

--- src/edu/clarkson/autograder/client/objects/StudentRowData.java
+++ src/edu/clarkson/autograder/client/objects/StudentRowData.java
@@ -9,7 +9,7 @@ public class StudentRowData {
 	private String name;
 	private List<Double> grades = new ArrayList<Double>();
 	
-	public StudentRowData(String studentName, ArrayList<Double> studentGrades) {
+	public StudentRowData(String studentName, List<Double> studentGrades) {
 		this.name = studentName;
 		this.grades.addAll(studentGrades);
 	}
@@ -18,7 +18,7 @@ public class StudentRowData {
 		return this.name;
 	}
 	
-	public ArrayList<Double> getGrades() {
-		return this.getGrades();
+	public double getGrade(int index) {
+		return grades.get(index);
 	}
 }
