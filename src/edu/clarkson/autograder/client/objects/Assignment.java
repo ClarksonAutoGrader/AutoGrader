9e5cdbc81425397b092ab98e734dcfe0ed82bbd6
Ryan Wood <woodrj@clarkson.edu>
2018-01-04 20:51:55 -0500

    #5 cleanup botched merge

--- src/edu/clarkson/autograder/client/objects/Assignment.java
+++ src/edu/clarkson/autograder/client/objects/Assignment.java
@@ -3,107 +3,59 @@ package edu.clarkson.autograder.client.objects;
 import java.io.Serializable;
 import java.util.Date;
 
-import edu.clarkson.autograder.client.Autograder;
-import edu.clarkson.autograder.client.widgets.Listable;
-
 @SuppressWarnings("serial")
-public class Assignment implements Listable, Serializable {
+public class Assignment implements Serializable, Comparable<Assignment> {
     private int id;
     private int cId;
-    private boolean visible;
     private String title;
-    private Date openTime;
-    private Date closeTime;
+	private Date dueDate;
 
     /**
-     * No argument constructor
-     */
-    public Assignment() {
-    }
-
-    /**
-     * Convenience constructor (temporary)
-     * 
-     * @param id
-     * @param cId
-     * @param visible
-     * @param title
-     * @param openTime
-     * @param closeTime
-     */
-    public Assignment(int id, int cId, boolean visible, String title, Date openTime, Date closeTime) {
-        super();
+	 * Constructor
+	 * 
+	 * @param id
+	 *            unique assignment id
+	 * @param cId
+	 *            parent course
+	 * @param title
+	 *            course title
+	 * @param dueDate
+	 *            assignments after due date cannot be worked on for a grade
+	 */
+	public Assignment(int id, int cId, String title, Date dueDate) {
         this.id = id;
         this.cId = cId;
-        this.visible = visible;
         this.title = title;
-        this.openTime = openTime;
-        this.closeTime = closeTime;
+		this.dueDate = dueDate;
     }
+
+	/**
+	 * Default constructor required for serialization
+	 */
+	public Assignment() {
+	}
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public int getcId() {
         return cId;
     }
 
-    public void setcId(int cId) {
-        this.cId = cId;
-    }
-
-    public boolean isVisible() {
-        return visible;
-    }
-
-    public void setVisible(boolean visible) {
-        this.visible = visible;
-    }
-
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+	public Date getDueDate() {
+		return dueDate;
     }
 
-    public Date getOpenTime() {
-        return openTime;
-    }
-
-    public void setOpenTime(Date openTime) {
-        this.openTime = openTime;
-    }
-
-    public Date getCloseTime() {
-        return closeTime;
-    }
-
-    public void setCloseTime(Date closeTime) {
-        this.closeTime = closeTime;
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    public String getDescription() {
-        StringBuilder builder = new StringBuilder(60);
-        builder.append("Due: ");
-        // builder.append(closeTime.getDay()).append(", ");
-        builder.append(closeTime.getMonth() + 1).append("/");
-        builder.append(closeTime.getDate()).append("/");
-        builder.append(closeTime.getYear() % 100);
-
-        return builder.toString();
-    }
-
-    @Override
-    public String getToken() {
-		return Autograder.formatIdToken(cId) + Autograder.formatIdToken(id);
-    }
+	/**
+	 * Sort assignments by due date, future to past
+	 */
+	@Override
+	public int compareTo(Assignment other) {
+		return getDueDate().compareTo(other.getDueDate());
+	}
 }
