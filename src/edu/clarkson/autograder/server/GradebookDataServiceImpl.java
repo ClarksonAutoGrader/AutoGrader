da47ee83148112997900bb20f43b77aede6947da
Ryan Wood <woodrj@clarkson.edu>
2018-01-07 15:43:00 -0500

    #41 - finished resultset processing algorithm

--- src/edu/clarkson/autograder/server/GradebookDataServiceImpl.java
+++ src/edu/clarkson/autograder/server/GradebookDataServiceImpl.java
@@ -3,7 +3,6 @@ package edu.clarkson.autograder.server;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
@@ -18,27 +17,7 @@ import edu.clarkson.autograder.client.services.GradebookDataService;
 @SuppressWarnings("serial")
 public class GradebookDataServiceImpl extends RemoteServiceServlet implements GradebookDataService {
 
-	private static ConsoleHandler LOG = new ConsoleHandler();
-	
-	// TODO: remove temporary hardcoded data values
-	private GradebookData temp = new GradebookData(
-			Arrays.asList(
-					"Homework 1", "Homework 2", "Homework 3", "Homework 4", "Homework 5",
-					"Homework 6", "Homework 7", "Homework 8", "Homework 9", "Homework 10"),
-			Arrays.asList(
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0))),
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0))),
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(21.0, 22.0, 23.0, 24.0, 25.0, 26.0, 27.0, 28.0, 29.0, 30.0))),
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(31.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 40.0))),
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(41.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 50.0))),
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(51.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 60.0))),
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(61.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 70.0))),
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(71.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 80.0))),
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(81.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 90.0))),
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(91.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 100.0))),
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(101.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 110.0))),
-					new StudentRowData("woodrj", new ArrayList<Double>(Arrays.asList(111.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 120.0)))
-			));
+	private static ConsoleHandler LOG = new ConsoleHandler();	
 
 	@Override
 	public GradebookData fetchGradebookData(int courseId) throws IllegalArgumentException {
@@ -59,40 +38,51 @@ public class GradebookDataServiceImpl extends RemoteServiceServlet implements Gr
 			
 			LOG.publish(new LogRecord(Level.INFO, "GradebookDataServiceImpl#process - begin"));
 			
-			List<String> assignmentNames = new ArrayList<String>();
-			List<StudentRowData> studentGrades = new ArrayList<StudentRowData>();
+			List<StudentRowData> studentRowDataList = new ArrayList<StudentRowData>();
 			
-			String currentUser = "";
-			String lastUser = "initial value";
+			String currentUsername = "initial value";
+			String lastUsername = "initial value";
+			double currentUserPoints = -1;
 			
 			List<Double> currentStudentPoints = new ArrayList<Double>();
 			
 			// iterate on rows
 			while (rs.next()) {
-				currentUser = rs.getString("enr_username");
+				
+				LOG.publish(new LogRecord(Level.INFO, "GradebookDataServiceImpl#getGradebookData - row iteration"));
+				
+				currentUsername = rs.getString("enr_username");
+				currentUserPoints = rs.getDouble("uw.points");
+				
+				LOG.publish(new LogRecord(Level.INFO, "GradebookDataServiceImpl#getGradebookData - processing user: " + currentUsername));
+				
 				// test if a new user
-				if (currentUser.equals(lastUser)) {
+				if (currentUsername.equals(lastUsername)) {
+					currentStudentPoints.add(currentUserPoints);
+				} else {
+					
+					if(!rs.isFirst()) {
+						studentRowDataList.add(new StudentRowData(lastUsername, currentStudentPoints));
+					}
+					
+					lastUsername = currentUsername;
+					
+					currentStudentPoints = new ArrayList<Double>();
 					currentStudentPoints.add(rs.getDouble("uw.points"));
 				}
-				else {
-					if(!rs.isFirst()) {
-						studentGrades.add(new StudentRowData(currentUser, currentStudentPoints));
-					}
-				}
-				// add assignment names to list
-				if (!assignmentNames.contains(rs.getString("assignment_title"))) {
-					assignmentNames.add(rs.getString("assignment_title"));
-				}
-				
-				lastUser = currentUser;
-				
-				currentStudentPoints = new ArrayList<Double>();
-				currentStudentPoints.add(rs.getDouble("uw.points"));
-				
+			}
+			studentRowDataList.add(new StudentRowData(lastUsername, currentStudentPoints));
+			
+			// add assignment names to list
+			ArrayList<String> assignmentNames = new ArrayList<String>();
+			rs.first();
+			String firstUser = rs.getString("enr_username");
+			assignmentNames.add(rs.getString("assignment_title") + " (" + rs.getDouble("prob.points_possible") + " points)");
+			while(rs.next() && rs.getString("enr_username").equals(firstUser)) {
+				assignmentNames.add(rs.getString("assignment_title") + " (" + rs.getDouble("prob.points_possible") + " points)");
 			}
 			
-			
-			GradebookData gradebookData = new GradebookData(assignmentNames, studentGrades);
+			GradebookData gradebookData = new GradebookData(assignmentNames, studentRowDataList);
 			
 			return gradebookData;
 		}
